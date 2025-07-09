627207c5dd6822a0c0b9975b3ff4bcf46dbefd86
mikeehow92 <gamer2022nubes@gmail.com>
2025-07-09 13:07:33 -0600

    feat: Configure admin panel for dedicated Firebase Hosting site

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
+    import { defineConfig } from 'vite';
+    import react from '@vitejs/plugin-react';
+    import path from 'path';
+    import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
 
-export default defineConfig({
-  root: path.resolve(__dirname, './'),
-  plugins: [react()],
-  base: '/admin/',
-  build: {
-    outDir: 'dist', 
-    assetsDir: 'admin/assets', 
-  },
-});
+    export default defineConfig({
+      root: path.resolve(__dirname, './'),
+      plugins: [react()],
+      base: '/', 
+      build: {
+        outDir: 'dist', 
+        assetsDir: 'assets', 
+      },
+    });
+    
